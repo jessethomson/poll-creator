444f7840b172485e04816c73b39bc7753ccf8863
Garrett Cox <coxmg@mtc.byu.edu>
2016-11-08 21:25:38 -0700

    added some steps for the poll page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+//Pretty sure we will need these
+// var mongoose = require('mongoose');
+// var Comment = mongoose.model('Comment');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/poll',function(req,res,next) {
+	// Comment.find(function(err, comments){
+ //    if(err){ return next(err); }
+ //    res.json(comments);
+ //  });
+	res.render('poll', { title: 'Express' });
+});
+
 
 module.exports = router;
